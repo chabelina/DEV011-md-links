fb5a373774e6920f5d0e162478447109a6b15485
Elizabeth <chabelina2003@gmail.com>
2023-12-05 13:22:50 -0500

    Created the test file for mdLinks function in index.js

--- test/md-links.spec.js
+++ test/md-links.spec.js
@@ -1,10 +1,10 @@
-const mdLinks = require('../');
+const mdLinks = require('../index.js');
 
 
 describe('mdLinks', () => {
 
-  it('should...', () => {
-    console.log('FIX ME!');
+  it('should return a promise', () => {
+    expect(mdLinks()).toBe(typeof Promise);
   });
 
 });
