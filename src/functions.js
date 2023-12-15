396b4e9d566e6ffb7554efbc71c9cbb3c605376e
Elizabeth <chabelina2003@gmail.com>
2023-12-15 14:23:19 -0500

    Added mdParser function and fileReader function to functions.js module

--- src/functions.js
+++ src/functions.js
@@ -1,6 +1,8 @@
 const path = require('path');
 const fs = require('fs');
 const fsPromises = fs.promises;
+const markdownIt = require('markdown-it');
+const md = new markdownIt();
 
 const pathConverter = (route) => {
   return path.isAbsolute(route) ? route : path.resolve(route);
@@ -15,14 +17,48 @@ const isMdFile = (absolutePath) => {
   return extension === '.md';
 }
 
-const readAndStore = (validatedPath) => {
-  fsPromises.readFile(validatedPath)
-    .then()
-    .catch()
+const mdParser = (content) => {
+  const parseTree = md.parse(content);
+  // console.log(JSON.stringify(parseTree, null, 2));
+  return parseTree;
+}
+
+const fileReader = (validatedPath) => {
+  return fsPromises.readFile(validatedPath, {encoding: 'utf8'})
+    .then((result) => {
+      const content = result;
+      const parsedFileContent = mdParser(content);
+      
+      let linkList = []
+
+      parsedFileContent.forEach((token) => {
+        let text;
+        let href;
+
+        const tokenContent = token.content;
+        const regex = /\[([^\]]+)\]\((https?:\/\/[^\s]+)\)/g;
+        let foundLinkData = regex.exec(tokenContent);
+
+        while(foundLinkData !== null){
+          text = foundLinkData[1];
+          href = foundLinkData[2];
+
+          linkList.push({href,text, file:validatedPath})
+          foundLinkData = regex.exec(tokenContent);
+        }
+      });
+      console.log(linkList);
+      return linkList;
+    })
+    .catch((error) => {
+      throw new Error('Could not read the file!');
+    })
 }
 
 module.exports = {
   pathConverter,
   isRealRoute,
   isMdFile,
+  fileReader,
+  mdParser
 };
