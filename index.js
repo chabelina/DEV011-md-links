b5f32d13bf501fa6b5c38e212546402430e21c1d
Elizabeth <chabelina2003@gmail.com>
2024-01-05 12:11:35 -0500

    mdLinks function update - Added two new arguments to mdLinks function: validate and stats - Stored the fileReader result in linkListPromise - Resolved linkListPromise with the linkListArray and or statResults depending on the optional arguments chosen by the user

--- index.js
+++ index.js
@@ -1,28 +1,69 @@
-const { pathConverter, isRealRoute, isMdFile, fileReader } = require("./src/functions.js");
+const {
+  pathConverter,
+  isRealRoute,
+  isMdFile,
+  fileReader,
+  httpStatusValidator,
+  statisticsCalculator,
+} = require("./src/functions.js");
 
-const mdLinks = (path) => {
-return new Promise((resolve, reject) => {
-  //check if path is absolute, convert if isn't. return the absolutePath
-  const absolutePath = pathConverter(path);
-  //validate if route directory exists in PC and if resulting file is Markdown
-  const isFound = isRealRoute(absolutePath);
-  const isMD = isMdFile(absolutePath);
+const mdLinks = (path, validate, stats) => {
+  return new Promise((resolve, reject) => {
+    //check if path is absolute, convert if isn't. return the absolutePath
+    const absolutePath = pathConverter(path);
+    //validate if route directory exists in PC and if resulting file is Markdown
+    const isFound = isRealRoute(absolutePath);
+    const isMD = isMdFile(absolutePath);
 
-  if(isFound && isMD){
-    //read md file and extract link array
-    fileReader(absolutePath);
+    if (isFound && isMD) {
+      //read md file and extract link array within resolved promise
+      const linkListPromise = fileReader(absolutePath);
 
-    //resolve the promise with the object of links
-    resolve(absolutePath)
-  } else if (!isFound) {
-    reject(new Error('The input path does not exist! No file found.'));
-  } else if (!isMD) {
-    reject(new Error('The input path leads to a non-markdown file! Please provide a valid file.'));
-  }
-  // console.log('archivo fue encontrado?',isFound);
-  //read md file
-})
-}
+      linkListPromise.then((linkListArray) => {
+        let aux = [];
+        //resolve the promise with the object of links depending on wether or not the
+        //validate param is being used
+        // const resultArr = []
+        linkListArray.forEach((object) => {
+          const httpValidationPromises = httpStatusValidator(object.href)
+          .then((res) => res)
+          .catch((error) => error);
+          
+          aux.push(httpValidationPromises);
+        });
+        
+        Promise.all(aux)
+        .then((httpValidationResults) => {
+          const statResults = statisticsCalculator(httpValidationResults);
 
-module.exports = mdLinks;
+          if (validate && !stats) {
+            httpValidationResults.forEach((data, index) => {
+              linkListArray[index].status = data.statCode;
+              linkListArray[index].ok = data.isOk;
+            });
+            resolve(linkListArray );
+          }
+          else if (stats && !validate) {
+            resolve(statResults);
+          }
+          else {
+            resolve({ statResults, linkListArray });
+          }
+         
+        });
+      });
+    } else if (!isFound) {
+      reject(new Error("The input path does not exist! No file found."));
+    } else if (!isMD) {
+      reject(
+        new Error(
+          "The input path leads to a non-markdown file! Please provide a valid file."
+        )
+      );
+    }
+    // console.log('archivo fue encontrado?',isFound);
+    //read md file
+  });
+};
 
+module.exports = { mdLinks };
